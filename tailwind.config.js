e7e211a7e978c62c1e142275a973972c57525214
Matt <MattAG@users.noreply.github.com>
2021-03-07 12:32:13 +0000

    style: updated colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
         extend: {
             colors: {
                 blue: {
-                    500: '#26619c'
+                    100: "#d4dfeb",
+                    200: "#a8c0d7",
+                    300: "#7da0c4",
+                    400: "#5181b0",
+                    500: "#26619c",
+                    600: "#1e4e7d",
+                    700: "#173a5e",
+                    800: "#0f273e",
+                    900: "#08131f"
                 },
                 grey: colours.gray,
                 orange: colours.orange
